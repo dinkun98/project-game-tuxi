8825602f54ab84b9f22b0325f89eb661b22876f6
unknown <dinkun98@gmail.com>
2022-09-20 15:10:23 +0700

    Updates

---
+++ precache-manifest.3d7d1e24c2608336ebc2bd80db7a620d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7aaed4ca38d3cb9fcc624680c3238b6",
+    "url": "/project-game-tuxi/index.html"
+  },
+  {
+    "revision": "b7fd1be05a86563bef25",
+    "url": "/project-game-tuxi/static/css/main.890ea209.chunk.css"
+  },
+  {
+    "revision": "550e8f422f5d67b5281b",
+    "url": "/project-game-tuxi/static/js/2.ebfa0dc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-game-tuxi/static/js/2.ebfa0dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7fd1be05a86563bef25",
+    "url": "/project-game-tuxi/static/js/main.6cda8849.chunk.js"
+  },
+  {
+    "revision": "3f07c539d8c3a3eb7de0",
+    "url": "/project-game-tuxi/static/js/runtime-main.61a84116.js"
+  },
+  {
+    "revision": "1716f3f87bf9af14a763249e77f202da",
+    "url": "/project-game-tuxi/static/media/Pony.1716f3f8.ttf"
+  },
+  {
+    "revision": "f0bb0ef5a660c01da834ab4b72c16eb6",
+    "url": "/project-game-tuxi/static/media/bgGameOanTuXi.f0bb0ef5.png"
+  }
+]);
